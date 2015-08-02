aaec48db572650dbb710663fdd496fc64dbc3ff5
Joshua Reibert <joshua.reibert@gmail.com>
2015-08-02 12:31:23 +0200

    Use cmake modules for requirements

---
+++ cmake/FindCVD.cmake
@@ -0,0 +1,3 @@
+
+find_path(CVD_INCLUDE_DIRS NAMES cvd/cvd_image.h)
+find_library(CVD_LIBRARIES NAMES libcvd.so)
---
+++ cmake/FindGVars3.cmake
@@ -0,0 +1,3 @@
+
+find_path(GVARS3_INCLUDE_DIRS NAMES gvars3/instances.h gvars3/GUI.h)
+find_library(GVARS3_LIBRARIES NAMES libGVars3.so)
