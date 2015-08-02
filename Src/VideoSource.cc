5b2b24ebd992674b4a567ae695333775971d2be6
Joshua Reibert <joshua.reibert@gmail.com>
2015-08-02 11:32:09 +0200

    Implement OpenCV video capture

--- Src/VideoSource.cc
+++ Src/VideoSource.cc
@@ -1,9 +1,41 @@
 #include "VideoSource.h"
 
-VideoSource::VideoSource() {};
+VideoSource::VideoSource()
+: mirSize(VIDEO_WIDTH, VIDEO_HEIGHT)
+, capture(0) {
+    if (!capture.isOpened())
+        std::cerr << "Failed to open video capture!" << std::endl;
+};
 
-void VideoSource::GetAndFillFrameBWandRGB(CVD::Image<CVD::byte> &imBW, CVD::Image<CVD::Rgb<CVD::byte> > &imRGB) {};
+VideoSource::~VideoSource() {}
+
+void VideoSource::GetAndFillFrameBWandRGB(
+  CVD::Image<CVD::byte> &imBW
+, CVD::Image<CVD::Rgb<CVD::byte> > &imRGB
+) {
+    cv::Mat rgbFrame, grayFrame;
+
+    // fetch camera frame and resize
+    capture.read(rgbFrame);
+    cv::resize(rgbFrame, rgbFrame, cv::Size(VIDEO_WIDTH, VIDEO_HEIGHT));
+
+    // extract rgb frame and store to rgb image parameter
+    cv::cvtColor(rgbFrame, rgbFrame, CV_BGR2RGB);
+    CVD::BasicImage<CVD::Rgb<CVD::byte> > cvdRgbFrame(
+        (CVD::Rgb<CVD::byte>*) rgbFrame.data,
+        CVD::ImageRef(rgbFrame.cols, rgbFrame.rows)
+        );
+    imRGB.copy_from(cvdRgbFrame);
+
+    // extract gray frame and store to gray image parameter
+    cv::cvtColor(rgbFrame, grayFrame, CV_RGB2GRAY);
+    CVD::BasicImage<CVD::byte> cvdGrayFrame(
+        (CVD::byte*) grayFrame.data,
+        CVD::ImageRef(grayFrame.cols, grayFrame.rows)
+        );
+    imBW.copy_from(cvdGrayFrame);
+};
 
 CVD::ImageRef VideoSource::Size() {
-    return CVD::ImageRef();
-};
+    return mirSize;
+}
--- Src/VideoSource.h
+++ Src/VideoSource.h
@@ -3,7 +3,7 @@
 //
 // VideoSource.h
 // Declares the VideoSource class
-// 
+//
 // This is a very simple class to provide video input; this can be
 // replaced with whatever form of video input that is needed.  It
 // should open the video input on construction, and provide two
@@ -12,20 +12,31 @@
 // a new frame and then overwrite the passed-as-reference images with
 // GreyScale and Colour versions of the new frame.
 //
+#include <iostream>
+
 #include <cvd/image.h>
 #include <cvd/byte.h>
 #include <cvd/rgb.h>
+#include <cvd/utility.h>
+
+#include <opencv2/opencv.hpp>
+
+#define VIDEO_WIDTH 640
+#define VIDEO_HEIGHT 480
 
 struct VideoSourceData;
 
-class VideoSource
-{
- public:
-  VideoSource();
-  void GetAndFillFrameBWandRGB(CVD::Image<CVD::byte> &imBW, CVD::Image<CVD::Rgb<CVD::byte> > &imRGB);
-  CVD::ImageRef Size();
-  
- private:
-  void *mptr;
-  CVD::ImageRef mirSize;
+class VideoSource {
+public:
+    VideoSource();
+    ~VideoSource();
+    void GetAndFillFrameBWandRGB(
+        CVD::Image<CVD::byte>& imBW, CVD::Image<CVD::Rgb<CVD::byte> >& imRGB
+        );
+    CVD::ImageRef Size();
+
+private:
+    void *mptr;
+    CVD::ImageRef mirSize;
+    cv::VideoCapture capture;
 };
